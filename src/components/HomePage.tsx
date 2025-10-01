aff77396c62ad928e03db4e677e4f0f724b1725f
waitlists6 <git5@fbi.monster>
2025-10-01 11:20:39 +0800

    Updated HomePage.tsx

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import React, { useState, useEffect } from "react"
 import { Search, TrendingUp } from "lucide-react"
 import { useNavigate, useParams, Link } from "react-router-dom"
